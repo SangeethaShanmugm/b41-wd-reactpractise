356bd5aa0039f4a0d7a4494d76170729f3aa9a6c
SangeethaShanmugm <sanjuriya22@gmail.com>
2022-12-20 09:57:12 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRoute>
-      <App />
-    </BrowserRoute>
+    <App />
   </React.StrictMode>
 );
 
